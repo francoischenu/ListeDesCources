7cb7bf28d964aa5d342f5af55fb56454aeddf90d
Francois <chenu.francois1@gmail.com>
2022-02-16 09:32:39 +0100

    First Commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "listedecourse560407bd": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "listedecourse": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/components/IngredientsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <h2>My Ingredients</h2>
+    <ul>
+      <li v-for="ingredient in ingredients" :key="ingredient.id">{{ ingredient.text }}</li>
+    </ul>
+    {{ingredients }}
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["ingredients"],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  margin: 1rem 0;
+  border: 1px solid #ccc;
+}
+</style>
